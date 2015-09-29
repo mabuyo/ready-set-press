afeaa23bf362c29e81f2a183ba5defe8f0229742
mabuyo <mabuyo@ualberta.ca>
2015-09-28 21:55:19 -0600

    Added dialog for single mode training instructions and navigation to single mode training.

---
+++ app/src/main/java/com/cmput301/mmabuyo/readysetpress/Dialog.java
@@ -0,0 +1,7 @@
+package com.cmput301.mmabuyo.readysetpress;
+
+/**
+ * Created by mmabuyo on 2015-09-24.
+ */
+public class Dialog {
+}
--- app/src/main/java/com/cmput301/mmabuyo/readysetpress/MainActivity.java
+++ app/src/main/java/com/cmput301/mmabuyo/readysetpress/MainActivity.java
@@ -1,9 +1,15 @@
 package com.cmput301.mmabuyo.readysetpress;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 
     @Override
@@ -33,5 +41,32 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    // http://www.tutorialspoint.com/android/android_alert_dialoges.htm
+    public void singleModeInstructions(View view) {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setTitle("Single Mode Training Instructions");
+        alertDialogBuilder.setMessage("Click the button when instructed to. Your reaction time will be recorded.");
+
+        alertDialogBuilder.setPositiveButton("Got it!", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface arg0, int arg1) {
+                Intent switchToTrainingMode = new Intent(MainActivity.this, SingleModeTrainingActivity.class);
+                startActivity(switchToTrainingMode);
+
+               // Toast.makeText(MainActivity.this,"You clicked yes button", Toast.LENGTH_LONG).show();
+            }
+        });
+
+//        alertDialogBuilder.setNegativeButton("No",new DialogInterface.OnClickListener() {
+//            @Override
+//            public void onClick(DialogInterface dialog, int which) {
+//                finish();
+//            }
+//        });
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
     }
 }
